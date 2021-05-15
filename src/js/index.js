eee422fde90854ec6944c45fc1d86d467218a9ec
Tomasz Gasowski <tomekgasowski93@gmail.com>
2021-05-15 18:05:22 +0200

    project adjustmets

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+const textareaInput = document.querySelector('.textarea--js');
+let result = '';
+const entry = localStorage.getItem('entry');
 
-/* place your code below */
+if (entry) {
+    result = entry;
+    textareaInput.value = result;
+}
 
-console.log('HELLO 🚀')
+const saveButton = document.querySelector('.save-button--js');
+saveButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry', textareaInput.value);
+})
+
+const loadButton = document.querySelector('.load-button--js');
+loadButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    textareaInput.value = localStorage.getItem('entry');
+})
