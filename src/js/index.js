f8c65a591041cde6c6dc61c034aaf6a530abe7c6
Tomek Gasowski <tomekgasowski93@gmail.com>
2022-01-24 17:02:31 +0100

    complete js

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,25 @@ import '../scss/main.scss';
 // registerSW();
 
 /* place your code below */
-let textArea = document.querySelector('.text-area--js');
 
+let textArea = document.querySelector('.text-area--js');
 let saveButton = document.querySelector('.button-save--js');
 let loadButton = document.querySelector('.button-load--js');
 
-saveButton.innerHTML = "safe";
-loadButton.innerHTML = "toad";
+let entry = localStorage.getItem('entry');
+let result = '';
+
+if (entry) {
+    result = entry;
+};
+
+textArea.value = result;
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('entry', textArea.value);
+});
+
+loadButton.addEventListener('click', () => {
+    console.log("asz ty w zyciu!");
+    textArea.value = localStorage.getItem('entry');
+});
